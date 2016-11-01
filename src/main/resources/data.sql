0affeaf63bfd7296c6dbe86365cfe6402cb79281
sandeepbupathi08 <sandeepbupathi08@gmail.com>
2016-11-01 08:17:00 -0400

    spell check

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 insert into STORE values (1,'ATL STR','3375 SPRING HILL');
 insert into STORE values (2,'SMYRNA STR','3372 SPRING HILL');
-insert into STORE values (3,'KNOXVELLI STR','3376 SPRING HILL');
+insert into STORE values (3,'knoxville STR','3376 SPRING HILL');
