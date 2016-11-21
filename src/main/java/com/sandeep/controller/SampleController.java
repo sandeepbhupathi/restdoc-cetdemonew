cc77c66e2181a8ac53fed4ac5b6f4a4ce26ba7d2
sandeepbupathi08 <sandeepbupathi08@gmail.com>
2016-11-20 20:56:49 -0500

    jdbc template samples

--- src/main/java/com/sandeep/controller/SampleController.java
+++ src/main/java/com/sandeep/controller/SampleController.java
@@ -1,7 +1,11 @@
 package com.sandeep.controller;
 
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
@@ -16,6 +20,8 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,10 +52,43 @@ public class SampleController {
 		return new ResponseEntity<>(assembler.toResource(strs),HttpStatus.OK);
 		
 	}
-	
+	@RequestMapping("/listStores")
+	public List<Store> listStores(){
+		
+		List<Store> strList = new ArrayList<Store>();
+		strList = jdbcTemp.query("select * from STORE", new RowMapper<Store>(){
+
+			@Override
+			public Store mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Store str = new Store();
+				str.setStoreNbr(rs.getInt(1));
+				str.setStrName(rs.getString(2));
+				str.setLastUpdTs(rs.getTimestamp(4));
+				str.setStrAddress(rs.getString(3));
+				return str;
+			}
+			
+		});
+		
+		Map<String,String> strMap = new HashMap<>();
+		
+		jdbcTemp.query("select * from STORE", new RowCallbackHandler(){
+
+			@Override
+			public void processRow(ResultSet rs) throws SQLException {
+				strMap.put(rs.getInt(1)+"|"+rs.getString(2), rs.getString(3));
+				
+			}
+			
+		});
+		
+		System.out.println("==========================="+strMap);
+		return strList;
+	}
 	@RequestMapping("/str")
 	public HttpEntity<Store> store(@RequestParam("storeNbr") int storeNbr){
 		Store strs= strRepo.findOne(storeNbr);
+		
 		logger.debug(jdbcTemp.queryForMap("select str_nm from STORE where str_nbr=1"));
 		return new ResponseEntity<>(strs,HttpStatus.OK);
 		
